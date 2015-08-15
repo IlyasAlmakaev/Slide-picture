a0322059720b0b150fd0771fb5205424c6865ed3
IlyasAlmakaev <i.almakaev@gmail.com>
2015-08-15 15:53:03 -0700

    Initial Commit

---
+++ Slide picture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Slide picture
+//
+//  Created by intent on 15/08/15.
+//  Copyright (c) 2015 intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
