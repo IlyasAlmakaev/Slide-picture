3968a21d163348f18c7de795bb8a8aa3d703149d
IlyasAlmakaev <i.almakaev@gmail.com>
2015-08-18 20:39:09 -0700

    Settings work

--- Slide picture/SettingsViewController.m
+++ Slide picture/SettingsViewController.m
@@ -9,6 +9,17 @@
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
+@property (weak, nonatomic) IBOutlet UISwitch *automaticSlide;
+@property (weak, nonatomic) IBOutlet UISwitch *showPicture;
+@property (weak, nonatomic) IBOutlet UISwitch *showRandom;
+@property (weak, nonatomic) IBOutlet UISwitch *showAnimation;
+@property (weak, nonatomic) IBOutlet UILabel *labelAutomaticSlide;
+@property (weak, nonatomic) IBOutlet UILabel *labelShowPicture;
+@property (weak, nonatomic) IBOutlet UILabel *labelShowRandom;
+@property (weak, nonatomic) IBOutlet UILabel *labelShowAnimation;
+@property (weak, nonatomic) IBOutlet UILabel *labelTimeInterval;
+@property (weak, nonatomic) IBOutlet UITextField *timeIntervalField;
+- (IBAction)switchPressed:(id)sender;
 
 @end
 
@@ -29,7 +40,11 @@
 // Сохранение настроек
 - (void)save
 {
-
+    NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
+    [settings setBool:self.automaticSlide.isOn forKey:@"automaticSlide"];
+    [settings setInteger:[self.timeIntervalField.text intValue] forKey:@"timeInterval"];
+    //[settings setBool:self.automaticSlide.isOn forKey:@"automaticSlide"];
+    [settings synchronize];
 }
 
 // Выход
@@ -39,4 +54,19 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (IBAction)switchPressed:(id)sender
+{
+    if (self.automaticSlide.on)
+        {
+        self.labelAutomaticSlide.text = @"Вкл. авт. смены картинок";
+        self.timeIntervalField.enabled = true;
+        self.labelTimeInterval.enabled = true;
+        }
+    else
+        {
+        self.labelAutomaticSlide.text = @"Выкл. авт. смены картинок";
+        self.timeIntervalField.enabled = false;
+        self.labelTimeInterval.enabled = false;
+        }
+}
 @end
