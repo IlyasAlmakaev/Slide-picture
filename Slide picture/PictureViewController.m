da22c433c3e587bb476344ce837e90af1540de3c
IlyasAlmakaev <i.almakaev@gmail.com>
2015-08-21 12:20:52 -0700

    step next view

--- Slide picture/PictureViewController.m
+++ Slide picture/PictureViewController.m
@@ -210,9 +210,10 @@
 // Метод перехода на следующую картинку
 - (void)nextShowViewController
 {
-    if (self.indexCurrent + 1 > _countPictures )
+    if (self.indexCurrent + 1 == _countPictures )
     {
     self.indexCurrent = 0;
+    
     }
     ShowViewController *initialViewController = [self viewControllerAtIndex:self.indexCurrent + 1];
 
